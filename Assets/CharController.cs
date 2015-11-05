7b14fe7747889568162aa6b4bedac4ceb33d8a41
Aaron Piotrowski <aaron.p.piotrowski@gmail.com>
2015-11-05 11:14:57 -0800

    mecanim state machine and code working together sort of

--- Assets/CharController.cs
+++ Assets/CharController.cs
@@ -4,9 +4,16 @@ using System.Collections;
 public class CharController : MonoBehaviour {
 	CharacterController controller;
 	float speed = 10f;
+	Animator animator;
+	bool u, d, l, r;
 
 	void Start () {
 		controller = GetComponent<CharacterController>();
+		animator = transform.Find("Animation").GetComponent<Animator>();
+		u = false;
+		d = false;
+		l = false;
+		r = false;
 	}
 	
 
@@ -16,32 +23,103 @@ public class CharController : MonoBehaviour {
 			controller.SimpleMove(new Vector3(0f, -speed, 0f));
 		}
 
+		u = false;
+		d = false;
+		l = false;
+		r = false;
+
+
 		//up
 		if (Input.GetKey (KeyCode.W)) {
+			u = true;
+		}
+		//down
+		if (Input.GetKey (KeyCode.S)) {
+			d = true;
+		}
+		//left
+		if (Input.GetKey (KeyCode.A)) {
+			l = true;
+		}
+		//right
+		if (Input.GetKey (KeyCode.D)) {
+			r = true;
+		}
+
+//		f(onlyUp()){
+//			keyOne = "u";
+//		}else if(onlyDown()){
+//			keyOne = "d";
+//		}else if(onlyLeft()){
+//			keyOne = "l";
+//		}else if(onlyRight()){
+//			keyOne = "r";
+//		}
+
+
+
+
+
+
+
+
+		//up
+		if (u) {
 			controller.SimpleMove(new Vector3(0f, 0f, speed));
+			animator.Play("GirlWalkUp");
 		} else if (Input.GetKeyUp (KeyCode.W)) {
 
 		}
 
 		//down
-		if (Input.GetKey (KeyCode.S)) {
+		if (d) {
 			controller.SimpleMove(new Vector3(0f, 0f, -speed));
+			animator.Play("GirlWalkDown");
 		} else if (Input.GetKeyUp (KeyCode.S)) {
 			
 		}
 
 		//left
-		if (Input.GetKey (KeyCode.A)) {
+		if (l) {
 			controller.SimpleMove(new Vector3(-speed, 0f, 0f));
+			animator.Play("GirlWalkLeft");
 		} else if (Input.GetKeyUp (KeyCode.S)) {
 			
 		}
 
 		//right
-		if (Input.GetKey (KeyCode.D)) {
+		if (r) {
 			controller.SimpleMove(new Vector3(speed, 0f, 0f));
+			animator.Play("GirlWalkRight");
 		} else if (Input.GetKeyUp (KeyCode.S)) {
-			
+		
 		}
+
+		if(Input.GetKeyUp(KeyCode.W)){
+			animator.SetTrigger("Idle");
+		}
+
+
 	}
+
+//	bool onlyUp(){
+//		if(u && !d && !r && !l){
+//			return true;
+//		}
+//	}
+//	bool onlyDown(){
+//		if(!u && d && !r && !l){
+//			return true;
+//		}
+//	}
+//	bool onlyLeft(){
+//		if(!u && !d && !r && l){
+//			return true;
+//		}
+//	}
+//	bool onlyRight(){
+//		if(!u && !d && r && !l){
+//			return true;
+//		}
+//	}
 }
