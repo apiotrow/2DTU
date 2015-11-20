8300d405b968719fadb8d8815fb134dacf06c090
Aaron Piotrowski <aaron.p.piotrowski@gmail.com>
2015-11-19 23:41:10 -0800

    sign attacking throttled via animation state machine settings

---
+++ Assets/MainCharController.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainCharController : MonoBehaviour {
+	CharacterController controller;
+	Animator animator;
+	float walkSpeed = 10f;
+	bool u, d, l, r;
+	float xGo, yGo, zGo;
+//	bool isWalking = false;
+
+	void Start () {
+		controller = GetComponent<CharacterController>();
+		animator = GetComponent<Animator>();
+	}
+	
+
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			animator.SetTrigger("attack");
+		}
+
+		u = false;
+		d = false;
+		l = false;
+		r = false;
+		xGo = 0f;
+		yGo = 0f;
+		zGo = 0f;
+//		isWalking = false;
+		animator.SetBool("walking", false);
+
+		if (Input.GetKey (KeyCode.A)) {
+			l = true;
+			animator.SetBool("walking", true);
+		}
+		if (Input.GetKey (KeyCode.S)) {
+			d = true;
+			animator.SetBool("walking", true);
+		}
+		if (Input.GetKey (KeyCode.W)) {
+			u = true;
+			animator.SetBool("walking", true);
+		}
+		if (Input.GetKey (KeyCode.D)) {
+			r = true;
+			animator.SetBool("walking", true);
+		}
+
+		if(l)
+			xGo = -walkSpeed;
+		if(d)
+			zGo = -walkSpeed;
+		if(r)
+			xGo = walkSpeed;
+		if(u)
+			zGo = walkSpeed;
+
+		controller.SimpleMove(new Vector3(xGo, yGo, zGo));
+	}
+}
