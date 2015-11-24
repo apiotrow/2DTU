bba5c425f9b19cb7cd04eaad5b45634094e79315
Aaron Piotrowski <aaron.p.piotrowski@gmail.com>
2015-11-23 22:44:32 -0800

    added tha pit and 5000gb of particle effects. fog changes color and density when nearing tha pit

--- Assets/CameraFocus.cs
+++ Assets/CameraFocus.cs
@@ -3,6 +3,14 @@ using System.Collections;
 
 public class CameraFocus : MonoBehaviour {
 	public Transform[] thingsToFocus;
+	public Transform thePit;
+	Color fogStartColor;
+	float fogInitialEndDistance;
+
+	void Start(){
+		fogStartColor = RenderSettings.fogColor;
+		fogInitialEndDistance = RenderSettings.fogEndDistance;
+	}
 
 	void Update () {
 		float avgX = 0f;
@@ -16,5 +24,14 @@ public class CameraFocus : MonoBehaviour {
 		Vector3 newCameraPos = new Vector3(avgX, transform.position.y, transform.position.z);
 
 		transform.position = Vector3.Lerp(transform.position, newCameraPos, Time.deltaTime);
+
+		if(Mathf.Abs((transform.position.x - thePit.position.x)) < 30f){
+			RenderSettings.fogColor = Color.Lerp(RenderSettings.fogColor, Color.black, Time.deltaTime);
+			RenderSettings.fogEndDistance = Mathf.Lerp(RenderSettings.fogEndDistance, 114f, Time.deltaTime);
+		}else{
+			RenderSettings.fogColor = Color.Lerp(RenderSettings.fogColor, fogStartColor, Time.deltaTime);
+			RenderSettings.fogEndDistance = Mathf.Lerp(RenderSettings.fogEndDistance, fogInitialEndDistance, Time.deltaTime);
+
+		}
 	}
 }
